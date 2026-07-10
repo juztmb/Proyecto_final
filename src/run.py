65d31e680b86cf54a0abeeb923e7dc340c6354b4
ELSAMUEL45 <cgutierrezol@unal.edu.co>
2026-07-10 11:23:17 -0500

    Implement CORS and static file middleware
    Added CORS middleware and static file serving.

--- src/run.py
+++ src/run.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from app.routes.jugador_routes import router as jugador_routes
 from app.routes import router_partido as partido_router
 from app.routes import router_administrador as admin_router
 
-
 app = FastAPI(title="API MVC con MongoDB (Sin Beanie)")
 
-# Incluimos las rutas (Vistas)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+
+
 app.include_router(jugador_routes)
 app.include_router(partido_router)
 app.include_router(admin_router)
 
-@app.get("/")
-def root():
-    return {"message": "API funcionando correctamente. Ve a /docs para probarla."}
+
+app.mount("/", StaticFiles(directory="src/static", html=True), name="static")
